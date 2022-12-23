4e01758d477daa3d03538fef703aa898b1842830
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-12-23 15:16:55 +0100

    Add HelpOffersModule to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+
+import { HelpOffersModule } from './help-offers/help-offers.module';
+
 import { AppController } from './app.controller';
+
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [HelpOffersModule],
   controllers: [AppController],
   providers: [AppService],
 })
