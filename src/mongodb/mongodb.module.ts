15e4dec344aea555cfc821b238f2a63e933639e2
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-12-26 10:15:47 +0100

    Add UniqueIdGeneratorService to MongodbModule providers

--- src/mongodb/mongodb.module.ts
+++ src/mongodb/mongodb.module.ts
@@ -4,6 +4,7 @@ import { Module } from '@nestjs/common';
 import { DB_INSTANCE, dbInstanceFactory } from './dependencies/db-instance';
 import { HelpOffersMongodbService } from './help-offers-mongodb.service';
 import { HELP_OFFERS_DB_SERVICE } from '../shared/dependencies/help-offers-db-service';
+import { UniqueIdGeneratorService } from './../shared/services/unique-id-generator.service';
 
 @Module({
   imports: [ConfigModule],
@@ -15,6 +16,7 @@ import { HELP_OFFERS_DB_SERVICE } from '../shared/dependencies/help-offers-db-se
     },
     HelpOffersMongodbService,
     { provide: HELP_OFFERS_DB_SERVICE, useExisting: HelpOffersMongodbService },
+    UniqueIdGeneratorService,
   ],
   exports: [HELP_OFFERS_DB_SERVICE],
 })
