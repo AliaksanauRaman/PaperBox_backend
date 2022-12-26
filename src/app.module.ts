370b7f69b7c7daf0303c9263255c61665ce41ce1
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-12-26 09:29:12 +0100

    Import ConfigModule to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { HelpOffersModule } from './help-offers/help-offers.module';
 
@@ -7,7 +8,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [HelpOffersModule],
+  imports: [ConfigModule.forRoot(), HelpOffersModule],
   controllers: [AppController],
   providers: [AppService],
 })
