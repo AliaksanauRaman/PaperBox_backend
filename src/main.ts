ad829c4699de4236bea9aef89bd590e927a3220b
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-12-26 09:43:54 +0100

    Enhance bootstrap function, add port from env and add some logging

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,24 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+const DEFAULT_PORT = 3000;
 
-  await app.listen(3000);
+async function bootstrap() {
+  const logger = new Logger('Bootstrap');
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const portFromEnvironment = configService.get<string>('PORT');
+  const finalPort = portFromEnvironment
+    ? parseInt(portFromEnvironment)
+    : DEFAULT_PORT;
+
+  await app.listen(finalPort);
+
+  logger.log('Server is started.');
+  logger.log(`Port: ${finalPort}.`);
 }
 
 bootstrap();
