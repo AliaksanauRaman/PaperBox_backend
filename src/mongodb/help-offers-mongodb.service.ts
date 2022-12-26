86ee7eabbe52d849eafb60bd0590db0400c5cc00
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-12-26 10:16:20 +0100

    Finish HelpOffersDbService interface and align HelpOffersMongodbService with it

--- src/mongodb/help-offers-mongodb.service.ts
+++ src/mongodb/help-offers-mongodb.service.ts
@@ -1,10 +1,14 @@
 import { Injectable, Inject } from '@nestjs/common';
-import { Db } from 'mongodb';
+import { Db, UpdateResult } from 'mongodb';
 
 import { DB_INSTANCE } from './dependencies/db-instance';
 
 import { HelpOffersDbService } from '../shared/dependencies/help-offers-db-service';
-import { HelpOfferDbRecordType } from './../shared/types/help-offer-db-record.type';
+import { UniqueIdGeneratorService } from './../shared/services/unique-id-generator.service';
+import { HelpOfferDbRecordType } from '../shared/types/help-offer-db-record.type';
+import { HelpOfferStatus } from '../shared/enums/help-offer-status.enum';
+import { CreateHelpOfferDto } from '../shared/dtos/create-help-offer.dto';
+import { UpdatedHelpOfferResponseType } from './../shared/types/updated-help-offer-response.type';
 
 const HELP_OFFERS_COLLECTION_NAME = 'help-offers';
 
@@ -18,33 +22,83 @@ export class HelpOffersMongodbService implements HelpOffersDbService {
   constructor(
     @Inject(DB_INSTANCE)
     private readonly mongodbInstance: Db,
+    private readonly uniqueIdGeneratorService: UniqueIdGeneratorService,
   ) {}
 
   public async getAll(): Promise<Array<HelpOfferDbRecordType>> {
     const cursor = await this.helpOffersCollection.find();
-    const helpOffers = await cursor.toArray();
-    return helpOffers;
+    const allHelpOffersDbRecords = await cursor.toArray();
+    return allHelpOffersDbRecords;
   }
 
-  public async getAllPublished(): Promise<unknown[]> {
-    throw new Error('Method not implemented.');
+  public async getAllPublished(): Promise<Array<HelpOfferDbRecordType>> {
+    const allHelpOffersDbRecords = await this.getAll();
+    return allHelpOffersDbRecords.filter(
+      ({ status }) => status === HelpOfferStatus.PUBLISHED,
+    );
   }
 
-  public async getOneById(helpOfferId: string): Promise<unknown> {
-    throw new Error('Method not implemented.');
+  public async getOneById(helpOfferId: string): Promise<HelpOfferDbRecordType> {
+    const helpOfferDbRecord = await this.helpOffersCollection.findOne({
+      _id: helpOfferId,
+    });
+    return helpOfferDbRecord;
   }
 
   public async createOneUnpublished(
-    createHelpOfferDto: unknown,
-  ): Promise<unknown> {
-    throw new Error('Method not implemented.');
+    createHelpOfferDto: CreateHelpOfferDto,
+  ): Promise<HelpOfferDbRecordType> {
+    const helpOfferId = this.uniqueIdGeneratorService.generate();
+    const helpOfferDbRecord: HelpOfferDbRecordType = {
+      _id: helpOfferId,
+      authorFullName: createHelpOfferDto.authorFullName,
+      countryFrom: createHelpOfferDto.countryFrom,
+      cityFrom: createHelpOfferDto.cityFrom,
+      countryTo: createHelpOfferDto.countryTo,
+      cityTo: createHelpOfferDto.cityTo,
+      status: HelpOfferStatus.UNPUBLISHED,
+      phones: createHelpOfferDto.phones,
+      date: createHelpOfferDto.date,
+      createdAt: new Date(),
+      lastModified: new Date(),
+      comment: createHelpOfferDto.comment,
+    };
+    await this.helpOffersCollection.insertOne(helpOfferDbRecord);
+    return helpOfferDbRecord;
   }
 
-  public async publishOne(helpOfferId: string): Promise<unknown> {
-    throw new Error('Method not implemented.');
+  public async publishOne(
+    helpOfferId: string,
+  ): Promise<UpdatedHelpOfferResponseType> {
+    await this.updateHelpOfferStatus(helpOfferId, HelpOfferStatus.PUBLISHED);
+    return { id: helpOfferId };
   }
 
-  public async unpublishOne(helpOfferId: string): Promise<unknown> {
-    throw new Error('Method not implemented.');
+  public async unpublishOne(
+    helpOfferId: string,
+  ): Promise<UpdatedHelpOfferResponseType> {
+    await this.updateHelpOfferStatus(helpOfferId, HelpOfferStatus.UNPUBLISHED);
+    return { id: helpOfferId };
+  }
+
+  public async rejectOne(
+    helpOfferId: string,
+  ): Promise<UpdatedHelpOfferResponseType> {
+    await this.updateHelpOfferStatus(helpOfferId, HelpOfferStatus.REJECTED);
+    return { id: helpOfferId };
+  }
+
+  private async updateHelpOfferStatus(
+    helpOfferId: string,
+    status: HelpOfferStatus,
+  ): Promise<UpdateResult> {
+    const updateResult = await this.helpOffersCollection.updateOne(
+      { _id: helpOfferId },
+      {
+        $set: { status },
+        $currentDate: { lastModified: true },
+      },
+    );
+    return updateResult;
   }
 }
--- src/shared/dependencies/help-offers-db-service/interface.ts
+++ src/shared/dependencies/help-offers-db-service/interface.ts
@@ -1,10 +1,15 @@
+import { UpdatedHelpOfferResponseType } from './../../types/updated-help-offer-response.type';
+import { CreateHelpOfferDto } from './../../dtos/create-help-offer.dto';
 import { HelpOfferDbRecordType } from '../../types/help-offer-db-record.type';
 
 export interface HelpOffersDbService {
   getAll(): Promise<Array<HelpOfferDbRecordType>>;
-  getAllPublished(): Promise<Array<unknown>>;
-  getOneById(helpOfferId: string): Promise<unknown>;
-  createOneUnpublished(createHelpOfferDto: unknown): Promise<unknown>;
-  publishOne(helpOfferId: string): Promise<unknown>;
-  unpublishOne(helpOfferId: string): Promise<unknown>;
+  getAllPublished(): Promise<Array<HelpOfferDbRecordType>>;
+  getOneById(helpOfferId: string): Promise<HelpOfferDbRecordType>;
+  createOneUnpublished(
+    createHelpOfferDto: CreateHelpOfferDto,
+  ): Promise<HelpOfferDbRecordType>;
+  publishOne(helpOfferId: string): Promise<UpdatedHelpOfferResponseType>;
+  unpublishOne(helpOfferId: string): Promise<UpdatedHelpOfferResponseType>;
+  rejectOne(helpOfferId: string): Promise<UpdatedHelpOfferResponseType>;
 }
