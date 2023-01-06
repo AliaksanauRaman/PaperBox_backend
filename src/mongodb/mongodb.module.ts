22c2854547cf10934b30f9adf4547d025061e711
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-06 10:39:19 +0100

    Add UsersMongodbService and USERS_DB_SERVICE to providers of MongodbModule, EncryptionModule to imports and USERS_DB_SERVICE to exports

--- src/mongodb/mongodb.module.ts
+++ src/mongodb/mongodb.module.ts
@@ -1,13 +1,17 @@
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 
+import { EncryptionModule } from '../encryption/encryption.module';
+
 import { DB_INSTANCE, dbInstanceFactory } from './dependencies/db-instance';
 import { HelpOffersMongodbService } from './help-offers-mongodb.service';
 import { HELP_OFFERS_DB_SERVICE } from '../shared/dependencies/help-offers-db-service';
-import { UniqueIdGeneratorService } from './../shared/services/unique-id-generator.service';
+import { UsersMongodbService } from './users-mongodb.service';
+import { USERS_DB_SERVICE } from '../shared/dependencies/users-db-service';
+import { UniqueIdGeneratorService } from '../shared/services/unique-id-generator.service';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, EncryptionModule],
   providers: [
     {
       provide: DB_INSTANCE,
@@ -16,8 +20,10 @@ import { UniqueIdGeneratorService } from './../shared/services/unique-id-generat
     },
     HelpOffersMongodbService,
     { provide: HELP_OFFERS_DB_SERVICE, useExisting: HelpOffersMongodbService },
+    UsersMongodbService,
+    { provide: USERS_DB_SERVICE, useExisting: UsersMongodbService },
     UniqueIdGeneratorService,
   ],
-  exports: [HELP_OFFERS_DB_SERVICE],
+  exports: [HELP_OFFERS_DB_SERVICE, USERS_DB_SERVICE],
 })
 export class MongodbModule {}
