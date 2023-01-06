ff2b1fc857ac5c4e5471a9c12bb5ef4bfe26459d
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-06 10:42:24 +0100

    Import UsersModule and AuthenticationModule to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,20 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { HelpOffersModule } from './help-offers/help-offers.module';
+import { UsersModule } from './users/users.module';
+import { AuthenticationModule } from './authentication/authentication.module';
 
 import { AppController } from './app.controller';
 
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule.forRoot(), HelpOffersModule],
+  imports: [
+    ConfigModule.forRoot(),
+    HelpOffersModule,
+    UsersModule,
+    AuthenticationModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
